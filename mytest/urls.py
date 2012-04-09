3e45249de37e20bc019d8695c1e7b09148c9dc68
ICTAutumnwm <autumn_sky_is@163.com>
2012-04-09 11:30:56 +0800

    Apr 6, 2012

--- mytest/urls.py
+++ mytest/urls.py
@@ -1,26 +1,28 @@
 from django.conf.urls import patterns, include, url
 from django.conf.urls.defaults import * 
 from django.conf import settings
-from mytest.views import hello, current_datetime, hours_ahead, mgt, title, tree, resource, app, frametest, info, info0, ajax, json, json1, jsontree
+from mytest.views import mgt, title, tree, mgtresource, noderesource, resource, app, jsonmgt, jsontree, jsonnode, createall, json, restart, json1, info, info0
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    (r'^hello/$', hello),
-    (r'^time/$', current_datetime),
-    (r'^info/$', info),
     (r'^mgt/$', mgt),
     (r'^mgt/title$', title),
     (r'^mgt/tree$', tree),
+    (r'^mgt/mgtresource$', mgtresource),
+    (r'^mgt/noderesource$', noderesource),
     (r'^mgt/resource$', resource),
     (r'^mgt/app$', app),
-    (r'^mgt/frametest$', frametest),
-    (r'^info0/$', info0),
-    (r'^ajax/$', ajax),
-    (r'^ajax/json/$', json),
     (r'^ajax/tree/$', jsontree),
+    (r'^ajax/jsonmgt/$', jsonmgt),
+    (r'^ajax/node/$', jsonnode),
+    (r'^ajax/createall/$', createall),
+    (r'^ajax/json/$', json),
+    (r'^ajax/restart/$', restart),
     (r'^ajax/json1/$', json1),
+
+
 
     # Examples:
     # url(r'^$', 'mytest.views.home', name='home'),
@@ -32,6 +34,6 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
     (r'^media/(?P<path>.*)$','django.views.static.serve',{'document_root':settings.STATIC_PATH}),
-    (r'^show/$', 'django.views.generic.simple.direct_to_template', {'template':'a.html'}),
+    #(r'^show/$', 'django.views.generic.simple.direct_to_template', {'template':'a.html'}),
     #(r'^cpu/$', 'django.views.generic.simple.direct_to_template', {'template':'cpu.html'}),
 )
--- mytest/views.py
+++ mytest/views.py
@@ -13,15 +13,8 @@ pre_cpu = 0
 pre_rx = 0
 pre_tx = 0
 flag = 1
-num = 0
+gnum = 0
 
-def hello(request):
-    return HttpResponse("Hello world")
-def current_datetime(request):
-    now = datetime.datetime.now()
-    return render_to_response('current_datetime.html', {'current_date':now})
-def ajax(request):
-    return render_to_response('live-server.html')
 class QuerySetEncoder( simplejson.JSONEncoder ):
     """
     Encoding QuerySet into JSON format.
@@ -31,31 +24,29 @@ class QuerySetEncoder( simplejson.JSONEncoder ):
             return serializers.serialize( "python", object,ensure_ascii = False )
         except:
             return simplejson.JSONEncoder.default( self, object )
-def info(request): 
-    return render_to_response('info.html')
+
 def mgt(request): 
     return render_to_response('mgt.html')
 def title(request): 
     return render_to_response('title.html')
 def tree(request): 
     return render_to_response('tree.html')
+def mgtresource(request): 
+    return render_to_response('mgtresource.html')
+def noderesource(request): 
+    return render_to_response('noderesource.html')
 def resource(request): 
     return render_to_response('resource.html')
 def app(request): 
     return render_to_response('app.html')
-def frametest(request): 
-    ip = request.GET.get('ip')
-    number = request.GET.get('number')
-    print ip
-    print number
-    return render_to_response('frametest.html')
-def json( request ):
+def json0( request ):
     print "calulate data"
     ip = request.GET.get('ip')
     number = request.GET.get('number')
     print ip
     print number 
-    cmd="./mainC list -n " + number + " -i " + ip
+    #cmd="./mainC list -n " + number + " -i " + ip
+    cmd="./mainC list -n 1 -i 127.0.0.1" 
     output = os.popen(cmd,'r')
     ret = output.read()
     info = ret.split(',')
@@ -97,6 +88,153 @@ def json( request ):
     #print result
     data = simplejson.dumps( data, cls = QuerySetEncoder )
     return HttpResponse( data )
+def jsontree( request ):
+    print "dtree"
+    global gnum
+    gnum += 1
+    sgnum = "%d "%gnum
+    cmd="./mainC getstate" 
+    output = os.popen(cmd,'r')
+    ret = output.read()
+    output.close()
+    m = 0
+    n = 0
+    data = []
+    #data = [{"id":m,"pid":0,"name":"Node1","url":"ip=127.0.0.1"},{"id":2,"pid":1,"name":"VM 1.1","url":"ip=127.0.0.1&number=1"},{"id":3,"pid":1,"name":"VM 1." + snum,"url":"ip=127.0.0.1&number=2"}]
+    l=len(ret)
+    ndcount = ret.count('node')
+    for i in range(0, ndcount):
+        #get node s e
+        nds = ret.find('node')
+        #print nds
+        ret = ret[nds+4:l]
+        nde = ret.find('node')
+        #print nde
+
+        #get node
+        ndstr = ret[0:nde]
+        vmcount = ndstr.count('demo')
+        ipend = ndstr.find('demo')
+        ip = ndstr[0:ipend]
+        m = m + 1
+        n = m
+        tmp = {"id":m,"pid":0,"name":ip,"url":"noderesource?ip="+ip}
+        data.append(tmp)
+
+        ndlen = len(ndstr)
+        l=len(ret)
+        ret = ret[nde:l]
+        for i in range(0,vmcount):
+	         #get vm s e
+	         vms = ndstr.find('demo')
+	         ndstr = ndstr[vms+4:ndlen]
+	         vme = ndstr.find('demo')
+	         if vme == -1:
+		          vme = ndlen
+	         #print "vme:%s" %vme
+	         #get vm
+	         num = ndstr[0:vme]
+	         m = m + 1
+	         tmp = {"id":m,"pid":n,"name":'VM'+num,"url":"resource?ip="+ip+"&number="+num}
+	         data.append(tmp)
+	         ndlen = len(ndstr)
+	         ndstr = ndstr[vme:ndlen]
+
+    #tmp = {"id":m+1,"pid":n,"name":"Node"+sgnum,"url":"ip=127.0.0.1"}
+    #data.append(tmp)
+    #print data
+    #tmp = {"id":2,"pid":1,"name":"VM 1.1","url":"ip=127.0.0.1&number=1"}
+    #data.append(tmp)
+    #tmp = {"id":3,"pid":1,"name":"VM 1." + snum,"url":"ip=127.0.0.1&number=2"}
+    #data.append(tmp)
+    data = simplejson.dumps( data, cls = QuerySetEncoder )
+    return HttpResponse( data )
+def jsonmgt( request ):
+    print "calulate data"
+    #ip = request.GET.get('ip')
+    #number = request.GET.get('number')
+    #print ip
+    #print number
+    cmd="./mgtinfo" 
+    output = os.popen(cmd,'r')
+    ret = output.read()
+    info = ret.split(',')
+    maxmem = string.atof(info[0])
+    mem = string.atof(info[1])
+    cpu_use = string.atof(info[2])
+    cpu_total = string.atof(info[3])
+    rb = string.atof(info[4])
+    tb = string.atof(info[5])
+    output.close()
+    data = [maxmem,mem,cpu_use,cpu_total,rb,tb]
+    data = simplejson.dumps( data, cls = QuerySetEncoder )
+    return HttpResponse( data )
+def jsonnode(request):
+    print "node data"
+    ip = request.GET.get('ip')
+    #number = request.GET.get('number')
+    print ip
+    #print number 
+    #cmd="./mainC list -n " + number + " -i " + ip
+    #cmd="./mainC list -n 1 -i 127.0.0.1" 
+    #output = os.popen(cmd,'r')
+    #ret = output.read()
+    #output.close()
+    data = [1]
+    #print result
+    data = simplejson.dumps( data, cls = QuerySetEncoder )
+    return HttpResponse( data )
+def createall( request ):
+    print "createall"
+    ip = request.GET.get('ip')
+    #number = request.GET.get('number')
+    print ip
+    #print number
+    #cmd="./mainC list -n " + number + " -i " + ip
+    #cmd="./mainC list -n 1 -i 127.0.0.1" 
+    #output = os.popen(cmd,'r')
+    #ret = output.read()
+    #output.close()
+    data = [1]
+    data = simplejson.dumps( data, cls = QuerySetEncoder )
+    return HttpResponse( data )
+def json(request):
+    print "calulate data"
+    ip = request.GET.get('ip')
+    number = request.GET.get('number')
+    print ip
+    print number 
+    cmd="./mainC list -n " + number + " -i " + ip
+    #cmd="./mainC list -n 1 -i 127.0.0.1" 
+    output = os.popen(cmd,'r')
+    ret = output.read()
+    info = ret.split(',')
+    maxmem = string.atof(info[0])
+    mem = string.atof(info[1])
+    cpunum = string.atof(info[2])
+    cpu = string.atof(info[3])
+    rx = string.atof(info[4])
+    tx = string.atof(info[5])
+    output.close()
+    data = [maxmem,mem,cpunum,cpu,rx,tx]
+    #print result
+    data = simplejson.dumps( data, cls = QuerySetEncoder )
+    return HttpResponse( data )
+def restart( request ):
+    print "restarting"
+    ip = request.GET.get('ip')
+    number = request.GET.get('number')
+    print ip
+    print number
+    #cmd="./mainC list -n " + number + " -i " + ip
+    #cmd="./mainC list -n 1 -i 127.0.0.1" 
+    #output = os.popen(cmd,'r')
+    #ret = output.read()
+    #output.close()
+    data = [1]
+    data = simplejson.dumps( data, cls = QuerySetEncoder )
+    return HttpResponse( data )
+
 def json1( request ):
     print "calulate data"
     ip = request.GET.get('ip')
@@ -106,18 +244,29 @@ def json1( request ):
     data = [1000002, 33232, 13, 23, 33, 100, 200]
     data = simplejson.dumps( data, cls = QuerySetEncoder )
     return HttpResponse( data )
-def jsontree( request ):
+def jsontree0( request ):
     print "dtree"
     global num
     num += 1
     snum = "%d "%num
     print num
-    m = 1
-    data = {"id":m,"pid":0,"name":"Node1","url":"ip=127.0.0.1"}
-    #,{"id":2,"pid":1,"name":"VM 1.1","url":"ip=127.0.0.1&number=1"}
-    #,{"id":3,"pid":1,"name":"VM 1." + snum,"url":"ip=127.0.0.1&number=2"}
+    cmd="./jsonlist" 
+    output = os.popen(cmd,'r')
+    ret = output.read()
+    m = 0
+    n = 0
+    #data = [{"id":1,"pid":0,"name":"Node1","url":"ip=127.0.0.1"},{"id":2,"pid":1,"name":"VM 1.1","url":"ip=127.0.0.1&number=1"},{"id":3,"pid":1,"name":"VM 1." + snum,"url":"ip=127.0.0.1&number=2"}]
+    data = []
+    tmp = {"id":1,"pid":0,"name":"Node1","url":"ip=127.0.0.1"}
+    data.append(tmp)
+    tmp = {"id":2,"pid":1,"name":"VM 1.1","url":"ip=127.0.0.1&number=1"}
+    data.append(tmp)
+    tmp = {"id":3,"pid":1,"name":"VM 1." + snum,"url":"ip=127.0.0.1&number=2"}
+    data.append(tmp)
     data = simplejson.dumps( data, cls = QuerySetEncoder )
     return HttpResponse( data )
+def info(request): 
+    return render_to_response('info.html')
 def info0(request):
     cpu0 = mem0 = 0
     cmd="./info"
@@ -134,12 +283,3 @@ def info0(request):
     print mem
     output.close()
     return render_to_response('info0.html', {'cpu':cpu, 'cpu0':cpu0, 'mem':mem, 'mem0':mem0})
-def hours_ahead(request, offset):
-    try:
-        offset = int(offset)
-    except ValueError:
-        raise Http404()
-    dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
-	 #assert False
-    html = "<html><body>In %s hour(s), it will be %s.</body></html>" % (offset, dt)
-    return HttpResponse(html)
